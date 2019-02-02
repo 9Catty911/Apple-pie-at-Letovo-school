7a2df7d816fcb9e6432c7f2c36fd4c16367b9ca7
Programming <Programming@macbook-pro-programming-7.local>
2019-02-02 15:04:41 +0300

    Initial Commit

---
+++ Vicelitsa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vicelitsa
+//
+//  Created by Programming on 02.02.2019.
+//  Copyright © 2019 Programming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
